5bbe91fa265d73ea0cf344007a20488b0cbf3d42
Jean <jadekler@gmail.com>
2013-10-08 21:00:42 -0400

    Incorrect but closer attempt

--- 15.lattice_paths/lattice_paths.go
+++ 15.lattice_paths/lattice_paths.go
@@ -2,8 +2,30 @@ package main
 
 import "fmt"
 
+type loc struct {
+    x, y int
+}
+
 func main() {
-    myArr := [][]int{ {1,2,3, 5}, {4,5,6} }
-   
-    fmt.Println(myArr)
+    myLoc := loc{0, 0}
+    total := traverse(myLoc, 3, 0)
+    fmt.Println(total)
+}
+
+func traverse(curLoc loc, maxSize int, total int) (int) {
+    if (curLoc.x == maxSize-1 && curLoc.y == maxSize-1) {
+        return 1;
+    }
+
+    fmt.Println(curLoc)
+
+    if (curLoc.x < maxSize-1) {
+        total += traverse(loc{curLoc.x+1, curLoc.y}, maxSize, total)
+    }
+
+    if (curLoc.y < maxSize-1) {
+        total += traverse(loc{curLoc.x, curLoc.y+1}, maxSize, total)
+    }
+
+    return total
 }
