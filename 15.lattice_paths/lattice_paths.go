d99a8cbdcb1a862fa3a09d5a9f3a3c8b9e137451
Jean <jadekler@gmail.com>
2013-10-08 21:57:06 -0400

    Brute force recursion attempt. Horrible run time complexity.

--- 15.lattice_paths/lattice_paths.go
+++ 15.lattice_paths/lattice_paths.go
@@ -8,24 +8,23 @@ type loc struct {
 
 func main() {
     myLoc := loc{0, 0}
-    total := traverse(myLoc, 3, 0)
+    total := 0
+    traverse(myLoc, 20, &total)
     fmt.Println(total)
 }
 
-func traverse(curLoc loc, maxSize int, total int) (int) {
-    if (curLoc.x == maxSize-1 && curLoc.y == maxSize-1) {
-        return 1;
-    }
-
+func traverse(curLoc loc, maxSize int, total *int) {
     fmt.Println(curLoc)
 
-    if (curLoc.x < maxSize-1) {
-        total += traverse(loc{curLoc.x+1, curLoc.y}, maxSize, total)
+    if (curLoc.x == maxSize && curLoc.y == maxSize) {
+        *total += 1
     }
 
-    if (curLoc.y < maxSize-1) {
-        total += traverse(loc{curLoc.x, curLoc.y+1}, maxSize, total)
+    if (curLoc.x < maxSize) {
+        traverse(loc{curLoc.x+1, curLoc.y}, maxSize, total)
     }
 
-    return total
+    if (curLoc.y < maxSize) {
+        traverse(loc{curLoc.x, curLoc.y+1}, maxSize, total)
+    }
 }
