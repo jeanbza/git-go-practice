295d553791435e8a0596883d8fad2c323c9f0c18
Jean <jadekler@gmail.com>
2013-10-13 19:30:11 -0400

    Added full map

--- 17.number_letters/number_letters.go
+++ 17.number_letters/number_letters.go
@@ -5,8 +5,35 @@ import "fmt"
 var numbers map[int]string
 
 func main() {
-	numbers = make(map[int]string)
-	numbers[1] = "one"
+    numbers = make(map[int]string)
+    numbers[1] = "one"
+    numbers[2] = "two"
+    numbers[3] = "three"
+    numbers[4] = "four"
+    numbers[5] = "five"
+    numbers[6] = "six"
+    numbers[7] = "seven"
+    numbers[8] = "eight"
+    numbers[9] = "nine"
+    numbers[10] = "ten"
+    numbers[20] = "twenty"
+    numbers[30] = "thirty"
+    numbers[40] = "forty"
+    numbers[50] = "fifty"
+    numbers[60] = "sixty"
+    numbers[70] = "seventy"
+    numbers[80] = "eighty"
+    numbers[90] = "ninety"
+    numbers[100] = "onehundred"
+    numbers[200] = "twohundred"
+    numbers[300] = "threehundred"
+    numbers[400] = "fourhundred"
+    numbers[500] = "fivehundred"
+    numbers[600] = "sixhundred"
+    numbers[700] = "sevenhundred"
+    numbers[800] = "eighthundred"
+    numbers[900] = "ninehundred"
+    numbers[1000] = "onethousand"
 
-	fmt.Println(numbers[1])
+    fmt.Println(numbers[1])
 }
