a7106298258914dc2f22871103924af7bf35bf25
Jean <jadekler@gmail.com>
2013-10-13 15:17:16 -0400

    Modified comments

--- 16.power_digit_sum/power_digit_sum.go
+++ 16.power_digit_sum/power_digit_sum.go
@@ -9,7 +9,8 @@ func main() {
     veryBigNum := big.NewInt(1)
     two := big.NewInt(2)
 
-    for i:=0; i<1000; i++ {
+    // Manually doing 2^1000
+    for i:=0; i<1000; i++ { 
         temp := new(big.Int)
         temp.Mul(veryBigNum, two)
         veryBigNum = temp
@@ -19,7 +20,7 @@ func main() {
     sum := 0
 
     for _, j := range veryBigNumString {
-        sum += int(j-48) // Because range returns ASCII table, and 48 = 0
+        sum += int(j-48) // Because range returns ASCII value, and 48 = 0
     }
 
     fmt.Println(sum)
