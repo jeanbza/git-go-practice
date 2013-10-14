a917a2b28cb544f7e1feda550a30459d19cca1e3
Jean <jadekler@gmail.com>
2013-10-13 20:09:04 -0400

    Added some progress, not quite there though

--- 17.number_letters/number_letters.go
+++ 17.number_letters/number_letters.go
@@ -2,38 +2,80 @@ package main
 
 import "fmt"
 
-var numbers map[int]string
+var ones map[int]string
+var tens map[int]string
+var hundreds map[int]string
+var thousands map[int]string
 
 func main() {
-    numbers = make(map[int]string)
-    numbers[1] = "one"
-    numbers[2] = "two"
-    numbers[3] = "three"
-    numbers[4] = "four"
-    numbers[5] = "five"
-    numbers[6] = "six"
-    numbers[7] = "seven"
-    numbers[8] = "eight"
-    numbers[9] = "nine"
-    numbers[10] = "ten"
-    numbers[20] = "twenty"
-    numbers[30] = "thirty"
-    numbers[40] = "forty"
-    numbers[50] = "fifty"
-    numbers[60] = "sixty"
-    numbers[70] = "seventy"
-    numbers[80] = "eighty"
-    numbers[90] = "ninety"
-    numbers[100] = "onehundred"
-    numbers[200] = "twohundred"
-    numbers[300] = "threehundred"
-    numbers[400] = "fourhundred"
-    numbers[500] = "fivehundred"
-    numbers[600] = "sixhundred"
-    numbers[700] = "sevenhundred"
-    numbers[800] = "eighthundred"
-    numbers[900] = "ninehundred"
-    numbers[1000] = "onethousand"
+    ones = make(map[int]string)
+    tens = make(map[int]string)
+    hundreds = make(map[int]string)
+    thousands = make(map[int]string)
 
-    fmt.Println(numbers[1])
+    ones[1] = "one"
+    ones[2] = "two"
+    ones[3] = "three"
+    ones[4] = "four"
+    ones[5] = "five"
+    ones[6] = "six"
+    ones[7] = "seven"
+    ones[8] = "eight"
+    ones[9] = "nine"
+    ones[10] = "ten"
+    ones[11] = "eleven"
+    ones[12] = "twelve"
+    ones[13] = "thirteen"
+    ones[14] = "fourteen"
+    ones[15] = "fifteen"
+    ones[16] = "sixteen"
+    ones[17] = "seventeen"
+    ones[18] = "eighteen"
+    ones[19] = "nineteen"
+    tens[2] = "twenty"
+    tens[3] = "thirty"
+    tens[4] = "forty"
+    tens[5] = "fifty"
+    tens[6] = "sixty"
+    tens[7] = "seventy"
+    tens[8] = "eighty"
+    tens[9] = "ninety"
+    hundreds[1] = "onehundred"
+    hundreds[2] = "twohundred"
+    hundreds[3] = "threehundred"
+    hundreds[4] = "fourhundred"
+    hundreds[5] = "fivehundred"
+    hundreds[6] = "sixhundred"
+    hundreds[7] = "sevenhundred"
+    hundreds[8] = "eighthundred"
+    hundreds[9] = "ninehundred"
+    thousands[1] = "onethousand"
+
+    for i := 1; i <= 120; i++ {
+        fmt.Println("")
+        fmt.Println(i)
+        fmt.Println((i/100)%10)
+        fmt.Println((i/10)%10)
+        fmt.Println(i%10)
+
+        if (i/100 > 0 && (i/100)%10 != 0) {
+            fmt.Printf("%v", hundreds[(i/100)%10])
+        }        
+
+        if (i >= 20 && i/10 > 0 && (i/10)%10 != 0) {
+            if (i >= 100) {
+                fmt.Printf("%v", "and")
+            }
+
+            fmt.Printf("%v", tens[(i/10)%10])
+        }
+
+        if (i < 20) {
+            fmt.Println(ones[i%20])
+        } else {
+            if (i%10 != 0) {
+                fmt.Println(ones[i%10])
+            }
+        }
+    }
 }
